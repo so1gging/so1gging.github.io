366b0be70f8bf81faa8dec53605846ff04b756f6
김솔지 <so1gging@gmail.com>
2025-09-16 22:46:36 +0900

    Updates

--- _next/static/chunks/8582b44b3feb1278.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/5fd082fcb803afac.js",
-  "static/chunks/3383141ac967393e.js",
-  "static/chunks/turbopack-0980de3c420de965.js"
-])
--- _next/static/chunks/c5ed1bf1450beddc.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/c00a928639e61e84.js",
-  "static/chunks/3383141ac967393e.js",
-  "static/chunks/turbopack-f01a6c80b9f9b51a.js"
-])
